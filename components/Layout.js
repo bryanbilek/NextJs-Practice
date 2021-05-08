49f26d46c306487009e44c581a7981fa02c0374f
bryanbilek <bbilek35@gmail.com>
2021-05-07 19:57:56 -0500

    resolved adding mp4 files in the background; added audio but need to resolve autoplay (called autoplay but on localhost it isnt working)

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Footer from "./Footer";
 import Nav from "./Nav";
-// import VideoPlayer from 'react-video-js-player'
+import ReactAudioPlayer from 'react-audio-player'
 
 const Layout = ({ children }) => {
   return (
     <div>
-      {/* <VideoPlayer src='https://youtu.be/LWJfxDkPB7E' width='100%' height='100%' autoplay /> */}
+      <video
+        src="/videos/video.mp4"
+        autoPlay
+        loop
+        muted
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "fixed",
+          zIndex: "-1",
+        }}
+      />
+      <ReactAudioPlayer src="audio/matrix.mp3" controls autoPlay />
       <Nav />
       {children}
       <Footer />
